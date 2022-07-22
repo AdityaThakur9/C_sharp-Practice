5a5a76770901ad30b8f8789d4e2a04886f3d7a48
AdityaThakur9 <81606731+AdityaThakur9@users.noreply.github.com>
2022-07-22 09:04:45 +0530

    Files from day 2

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Employee
+    {
+        int Empid;
+        public string Empname;
+        DateTime DOJ;
+        float salary;
+
+        public void Accept()
+        {
+            Console.WriteLine("Enter the Employee nID, name, DOJ, and salary: ");
+            Empid=Convert.ToInt32(Console.ReadLine());
+            Empname=Console.ReadLine();
+            DOJ=Convert.ToDateTime(Console.ReadLine());
+            salary=Convert.ToSingle(Console.ReadLine());
+        }
+        public void display()
+        {
+            Console.WriteLine($"EmpID: {Empid},Name:{Empname},DOJ: {DOJ},Salary: {salary}");
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/ConsoleApp2/Project2Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+namespace Project2
+{
+    class Project2Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp= new Employee();
+            emp.Empname="Mohit";
+            Console.WriteLine(emp.Empname);
+            emp.Accept();
+            emp.display();
+            Console.Read();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee= new Employee();
+            employee.Accept();
+            employee.display();
+            employee.Empname="Shinde";
+            Console.Read();
+        }
+    }
+}
